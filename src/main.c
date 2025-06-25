5cf4fa2aef03923d9f47a4bed853f55a4b7b03ee
haruki7049 <tontonkirikiri@gmail.com>
2025-06-25 15:39:59 +0900

    Update

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 #include <SDL3/SDL_render.h>
 #include <stdio.h>
 
+SDL_FRect init_frect(float x, float y, float w, float h) {
+  SDL_FRect result;
+  result.x = x;
+  result.y = y;
+  result.w = w;
+  result.h = h;
+
+  return result;
+}
+
 int main() {
   const int window_w = 1280;
   const int window_h = 720;
@@ -24,18 +34,44 @@ int main() {
     return 1;
   }
 
-  // Drawing
-  SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderPresent(renderer);
-
   SDL_Event event;
+  float example_rect_x = 100;
+  float example_rect_y = 100;
 
   // Main loop
   while (true) {
+    // Drawing
+    SDL_SetRenderDrawColor(renderer, 255, 100, 20, 255);
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    const SDL_FRect example_rect = init_frect(example_rect_x, example_rect_y, 100.0, 100.0);
+    SDL_RenderFillRect(renderer, &example_rect);
+    SDL_RenderPresent(renderer);
+
+
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_EVENT_QUIT)
         return 0;
+
+      if (event.type == SDL_EVENT_KEY_DOWN) {
+        if (event.key.key == SDLK_ESCAPE)
+          return 0;
+
+        if (event.key.key == SDLK_LEFT) {
+          example_rect_x = example_rect_x - 10;
+	}
+        if (event.key.key == SDLK_RIGHT) {
+          example_rect_x = example_rect_x + 10;
+	}
+        if (event.key.key == SDLK_UP) {
+          example_rect_y = example_rect_y - 10;
+	}
+        if (event.key.key == SDLK_DOWN) {
+          example_rect_y = example_rect_y + 10;
+	}
+      }
     }
   }
 
